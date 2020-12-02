d3004cd6f2f85ef6ea8aba989198701e622a1d79
mahmoud <mahmoudhamra@gmail.com>
2020-12-02 10:37:59 -0500

    added a logo

--- cc.py
+++ cc.py
@@ -1,3 +1,5 @@
+from art import logo
+
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
 'v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
 'v','w','x','y','z']
@@ -17,7 +19,7 @@ def caesar(message, shift_val, mechanism):
     print(f"The {mechanism}d message is: {final_message}")
 
 
-
+print(logo)
 while True:
 
     choice = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
