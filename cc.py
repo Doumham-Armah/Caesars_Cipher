f5c602fdad7e7a0248b458fc829efa8d0a6da53a
mahmoud <mahmoudhamra@gmail.com>
2020-12-02 11:29:00 -0500

    final version

--- cc.py
+++ cc.py
@@ -1,23 +1,28 @@
 from art import logo
 
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
+'v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
 'v','w','x','y','z']
+
+print(len(letters))
 
 
 def caesar(message, shift_val, mechanism):
     final_message = ''
 
-    if shift_val > len(letters):
+    if shift_val >= len(letters):
         shift_val = shift_val% len(letters)
 
     if mechanism  == 'decode':
         shift_val *= -1
 
     for letter in message:
-        if letter != ' ':
+        if letter in letters:
             final_message += letters[letters.index(letter) + shift_val]
         else:
-            final_message += ' '
+            final_message += letter 
+
+
 
     print(f"The {mechanism}d message is: {final_message}")
 
@@ -25,14 +30,15 @@ def caesar(message, shift_val, mechanism):
 print(logo)
 while True:
 
-    choice = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+    choice = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower().strip()
+    print("your choice is: ", choice)   
 
     if choice == 'encode':
         message = input("Enter message to encrypt: ")
         shift_val = int(input("Enter shift value: "))
         caesar(message, shift_val, choice)
-    elif choice == 'decode':
+    elif choice == 'decode':     
         message = input("Enter message to decrypt: ")
-        shift_val = int(input("Enter shift value: "))
+        shift_val = int(input("Enter shift value: "))    
         caesar(message, shift_val, choice)
 
